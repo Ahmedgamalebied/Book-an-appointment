c0de56fb8291237545c2500d0d6cb87a9c6fc37f
Ahmedgamalebied <safaagamalebied@gmail.com>
2025-03-11 01:53:45 +0200

    add textStle and text

--- lib/Features/Home/presentation/views/widget/homeViewBody.dart
+++ lib/Features/Home/presentation/views/widget/homeViewBody.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/core/utils/assetsPath.dart';
+import 'package:flutter_application/core/utils/styles.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'Custom_Card.dart';
 import 'customAppbar.dart';
+import 'featuredlist_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -12,27 +14,19 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Expanded(
       child: Column(
-        children: [CustomAppBar(), FeaturedBooksListview()],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListview(),
+          SizedBox(
+            height: 45,
+          ),
+          Text(
+            "Best Seller",
+            style: Styles.titleMidem,
+          )
+        ],
       ),
-    );
-  }
-}
-
-class FeaturedBooksListview extends StatelessWidget {
-  const FeaturedBooksListview({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, item) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: CustomListViewItem(),
-            );
-          }),
     );
   }
 }
